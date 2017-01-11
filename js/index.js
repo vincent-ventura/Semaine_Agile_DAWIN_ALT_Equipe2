638b360f8d3f31985a6b2c98acafcee08864e740
vventura <vincent.ventura1@gmail.com>
2017-01-11 10:34:35 +0100

    gestion deplacement sans anim

--- js/index.js
+++ js/index.js
@@ -8,7 +8,17 @@ var largeurPlateau = canvas.width, // Largeur max = largeur du canvas
 	nombreCases = 49, // Le nombre de cases total est égal aux nombres de cases sur la largeur multipliés par le nombre de cases sur la hauteur
 	nombreCasesParLigne = 7, // Nombre de case pour chaque ligne du plateau
 	tailleCase = largeurPlateau/nombreCasesParLigne, // Taille d'une case du plateau
-	listeCases = []; // Contient un tableau avec la liste des cases
+	listeCases = [], // Contient un tableau avec la liste des cases
+	iJoueurs = Math.floor(nombreCases / 2), // Position des joueurs dans la liste des cases (init au milieu du plateau)
+	listeCanvas = [];
+
+function Joueur(nom) {
+	this.score = 0;
+	this.nom = nom;
+} 
+
+var joueur1 = new Joueur("Joueur 1"),
+	joueur2 = new Joueur("Joueur 2");
 
 // On créé une fonction pour crée chaque case du plateau
 function creerPlateau() {
@@ -16,13 +26,13 @@ function creerPlateau() {
 	context.fillRect(0, 0, largeurPlateau, hauteurPlateau); // On utilise la totalité du canvas pour créer nos cases
 
 	var colonne = 0,
-			ligne = 0; // On utilise ces variables pour changer les colonnes et lignes des différentes cases crées, on commence à 0 x 0 pour la première case
+		ligne = 0; // On utilise ces variables pour changer les colonnes et lignes des différentes cases crées, on commence à 0 x 0 pour la première case
 
 	// Pour chaque case du plateau :
 	for (var i = 0; i < nombreCases; i++) {
 		// On crée un carré de bordure noire de taille tailleCase*tailleCase
-		context.strokeStyle = 'grey';
-		context.strokeRect(tailleCase * colonne, tailleCase * ligne, tailleCase, tailleCase);
+		context.fillStyle = (i%2 === 0 ? 'white' : 'black');
+		context.fillRect(tailleCase * colonne, tailleCase * ligne, tailleCase, tailleCase);
 
 		// On ajoute un objet à chaque case avec un type et les positions
 		listeCases[i] = {
@@ -66,9 +76,9 @@ for (var i = 0; i < nombreObstacles; i++) {
 	}
 }
 
-listeCases[24].type = "joueur";
+listeCases[iJoueurs].type = "joueur";
 
-// Il y a 49 cases, on vérifie l'type de chacune et si c'est un piece, on colore la case en gris
+// Il y a 49 cases, on vérifie l'type de chacune et si c'est une piece, on colore la case en gris
 for (var i = 0; i < nombreCases; i++) {
 	(function(i) {
 		if (listeCases[i].type === "piece") {
@@ -77,6 +87,7 @@ for (var i = 0; i < nombreCases; i++) {
 			canvas.addEventListener('load', function() {
 				context.drawImage(canvas, listeCases[i].positionX, listeCases[i].positionY, 58, 58);
 			}, false);
+			listeCanvas[i] = canvas;
 		}
 		if (listeCases[i].type === "joueur"){
 			var canvas = new Image();
@@ -84,6 +95,45 @@ for (var i = 0; i < nombreCases; i++) {
 			canvas.addEventListener('load', function() {
 			context.drawImage(canvas, listeCases[i].positionX, listeCases[i].positionY);
 			}, false);
+			listeCanvas[i] = canvas;
 		}
 	})(i);
 }
+
+function deplacementValide( iCase ) {
+	return ( 
+		listeCases[iCase].type === "piece" && // return true si la pièce de destination est une piece ET
+		( listeCases[iCase].positionX === listeCases[iJoueurs].positionX || // que la piece soit sur la meme ligne que le joueur OU
+			listeCases[iCase].positionY === listeCases[iJoueurs].positionY ) // que la piece soit sur la meme colonne que le joueur
+		); 
+}
+
+function deplacerJoueurs(iCase) {
+	// Mettre à jour les cases
+	listeCases[iJoueurs].type = 'casevide';
+	iJoueurs = iCase;
+	listeCases[iJoueurs].type = 'joueur';
+
+	// faire disparaitre la piece située sur la case de destination
+	context.clearRect(listeCases[iCase].positionX, listeCases[iCase].positionY, tailleCase-2, tailleCase-2);
+
+	// effectuer le déplacement
+
+}
+
+function allerA( iCase ) {
+	if( deplacementValide(iCase) ) {
+		deplacerJoueurs(iCase);
+	}
+}
+
+function positionClic( x, y ) {
+	var ligne = Math.floor(y / tailleCase),
+		colonne = Math.floor(x / tailleCase);
+
+	return ligne*nombreCasesParLigne + colonne;
+}
+
+canvas.addEventListener('click', function (e) {
+	allerA( positionClic(e.offsetX, e.offsetY) );
+});
